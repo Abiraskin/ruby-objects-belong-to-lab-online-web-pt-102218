f1fa81c4a7f79fb28d4b39722869af387810e560
Abi Raskin <abiraskin@gmail.com>
2018-11-21 02:15:33 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,6 @@
 class Song
 
-  attr_reader :name
+  attr_accessor :title, :artist
 
-  def initialize(title, name)
-    @title = title
-    @name = name
-    @gnere = genre
-  end
 
 end
-
-dude = Artist.new("Cool Beans", "Dude")
-cool beans = Song.new("Coll Beans")
